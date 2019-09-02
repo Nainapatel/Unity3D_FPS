7d28714b2fe651de67df01f2510e740a6c2fdc3a
Naimish Kher <naimishkher95@gmail.com>
2019-09-02 16:25:26 +0530

    Health & Game Over

--- Assets/Scripts/ZombieFollow.cs
+++ Assets/Scripts/ZombieFollow.cs
@@ -10,6 +10,13 @@ public class ZombieFollow : MonoBehaviour {
     public int AttackTrigger;
     public RaycastHit Shot;
 
+    public int IsAttacking;
+    public GameObject ScreenFlash;
+    public AudioSource Hurt01;
+    public AudioSource Hurt02;
+    public AudioSource Hurt03;
+    public int PainSound;
+
     void Update () {
         transform.LookAt (ThePlayer.transform);
         if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out Shot)){
@@ -27,6 +34,9 @@ public class ZombieFollow : MonoBehaviour {
             }
         }
         if(AttackTrigger == 1) {
+            if(IsAttacking == 0){
+                StartCoroutine (EnemyDamage ());
+            }
             EnemySpeed = 0;
             TheEnemy.GetComponent<Animation> ().Play ("Attack");
         }
@@ -36,5 +46,27 @@ public class ZombieFollow : MonoBehaviour {
     }
     void OnTriggerExit () {
         AttackTrigger = 0;    
+    }
+
+    IEnumerator EnemyDamage () {
+        IsAttacking = 1;
+        PainSound = Random.Range (1,4);
+        yield return new WaitForSeconds(0.9f);
+        ScreenFlash.SetActive (true);
+        GlobalHealth.PlayerHealth -= 1;
+
+        if (PainSound == 1) {
+            Hurt01.Play();
+        }
+        if (PainSound == 2) {
+            Hurt02.Play();
+        }
+        if (PainSound == 3) {
+            Hurt03.Play();
+        }   
+        yield return new WaitForSeconds(0.05f);
+        ScreenFlash.SetActive (false);
+        yield return new WaitForSeconds (1);
+        IsAttacking = 0;
     }
 }
