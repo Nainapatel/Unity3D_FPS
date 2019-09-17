697dc862205bcde0e91df4a2e01906b2cdb4c53e
Nainapatel <happybhalodiya98@gmail.com>
2019-09-17 16:40:19 +0530

    Changes completed

--- Assets/Scripts/AmmoPickup.js
+++ Assets/Scripts/AmmoPickup.js
@@ -2,10 +2,10 @@ var AmmoSound : AudioSource;
 function OnTriggerEnter (col : Collider) {
     AmmoSound.Play();
     if (GlobalAmmo.LoadedAmmo == 0 ){
-        GlobalAmmo.LoadedAmmo += 10;
+        GlobalAmmo.LoadedAmmo += 90;
         this.gameObject.SetActive(false);
     }else{
-        GlobalAmmo.CurrentAmmo += 10;
+        GlobalAmmo.CurrentAmmo += 90;
         this.gameObject.SetActive(false);
     }
     
--- Assets/Scripts/AmmoPickupSMG.js
+++ Assets/Scripts/AmmoPickupSMG.js
@@ -2,10 +2,10 @@ var AmmoSound : AudioSource;
 function OnTriggerEnter (col : Collider) {
     AmmoSound.Play();
     if (GlobalAmmo.LoadedAmmo == 0 ){
-        GlobalAmmo.LoadedAmmo += 30;
+        GlobalAmmo.LoadedAmmo += 90;
         this.gameObject.SetActive(false);
     }else{
-        GlobalAmmo.CurrentAmmo += 30;
+        GlobalAmmo.CurrentAmmo += 90;
         this.gameObject.SetActive(false);
     }
 }
