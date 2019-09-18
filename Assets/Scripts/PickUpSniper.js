2f2c19ab8bee9f6b0f2ac08fabb6427e7383036c
Nainapatel <happybhalodiya98@gmail.com>
2019-09-18 19:08:32 +0530

    Sniper Gun added

---
+++ Assets/Scripts/PickUpSniper.js
@@ -0,0 +1,43 @@
+var TheDistance : float = PlayerCasting.DistanceFronTarget;
+var TextDisplay : GameObject;
+
+var FakeGun : GameObject;
+var RealGun : GameObject;
+var AmmoDisplay : GameObject;
+var PickUpAudio : AudioSource;
+
+var ObjectiveComplete : GameObject;
+var DoorStopper : GameObject;
+
+function Update () {
+    TheDistance = PlayerCasting.DistanceFronTarget;
+    
+}
+
+function OnMouseOver () {
+    if (TheDistance <= 2) {
+        TextDisplay.GetComponent.<Text>().text = "Take Sniper";
+    }
+    if (Input.GetButtonDown("Action")) {
+        if(TheDistance <= 2){
+            TakeNineMil();
+            ObjectiveComplete.SetActive(true);
+         }
+    }
+
+}
+
+function OnMouseExit () {
+    TextDisplay.GetComponent.<Text>().text = "" ;
+}
+
+function TakeNineMil () {
+
+    PlayerPrefs.SetInt("TakenAGun", 1);
+    DoorStopper.SetActive(false);
+    PickUpAudio.Play();
+    transform.position = Vector3(0, -1000, 0);
+    FakeGun.SetActive(false);
+    RealGun.SetActive(true);
+    AmmoDisplay.SetActive(true);
+}
